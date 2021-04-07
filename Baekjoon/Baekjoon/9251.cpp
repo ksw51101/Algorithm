9d216ab70bbae04cf908ef842430dbf508d86479
ksw51101 <52817679+ksw51101@users.noreply.github.com>
2021-04-07 14:35:07 +0900

    init

--- Baekjoon/Baekjoon/9251.cpp
+++ Baekjoon/Baekjoon/9251.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
 
+string A, B;
+int arr[1001][1001] = { 0, };
+
 int main() {
+	cin >> A >> B;
 
-	cout << 'a' << endl;
+	for (int i = 1; i <= A.length(); i++)
+		for (int j = 1; j <= B.length(); j++) {
+			if (A[i-1] == B[j-1])
+				arr[i][j] = arr[i - 1][j - 1] + 1;
+			else 
+				arr[i][j] = max(arr[i - 1][j], arr[i][j - 1]);
+		}
 
+	cout << arr[A.length()][B.length()] << '\n';
 	return 0;
 }
