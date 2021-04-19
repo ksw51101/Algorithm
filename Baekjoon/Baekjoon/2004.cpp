b52b0af07144abc92ec70dd976824a5bb00a7bdc
ksw51101 <52817679+ksw51101@users.noreply.github.com>
2021-04-20 00:42:27 +0900

    solved

--- Baekjoon/Baekjoon/2004.cpp
+++ Baekjoon/Baekjoon/2004.cpp
@@ -9,10 +9,10 @@ int num_cnt(int num, int obj) {
 	int cnt = 0, index = 0;
 	while (!(num % obj == 0))
 		num--;
-	if (num != 0) 
+	if (num != 0)
 		while (!(num >= pow(obj, index) && num < pow(obj, index + 1)))
 			index++;
-	
+
 	for (int i = 1; i <= index; i++) {
 		int r = pow(obj, i);
 		cnt += num / r;
@@ -22,13 +22,9 @@ int num_cnt(int num, int obj) {
 }
 
 int main() {
-	int a, b, c;
 	cin >> n >> m;
 
-	a = num_cnt(n, 2);
-	b = num_cnt(n - m, 2);
-	c = num_cnt(m, 2);
 	cout << min(num_cnt(n, 2) - num_cnt(n - m, 2) - num_cnt(m, 2), num_cnt(n, 5) - num_cnt(n - m, 5) - num_cnt(m, 5)) << '\n';
-	
+
 	return 0;
 }
