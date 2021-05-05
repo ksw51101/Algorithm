20752f66d3e8e75713228c0595e1a9aa0ac96820
ksw51101 <52817679+ksw51101@users.noreply.github.com>
2021-05-05 14:14:13 +0900

    add

---
+++ Practice/Practice/15-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, x;
+vector<int> vec;
+
+int main() {
+	cin >> n >> x;
+	
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		vec.push_back(a);
+	}
+	vector<int>::iterator upper = upper_bound(vec.begin(), vec.end(), x);
+	vector<int>::iterator lower = lower_bound(vec.begin(), vec.end(), x);
+
+	cout << upper - vec.begin();
+	/*int ans = upper - lower;
+	if (ans == 0)
+		cout << -1 << '\n';
+	else
+		cout << ans << '\n';*/
+
+	return 0;
+}
