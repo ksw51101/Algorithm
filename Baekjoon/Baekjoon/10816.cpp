e83f2036f4b98075b2ff0714850e6fbddf81aba1
ksw51101 <52817679+ksw51101@users.noreply.github.com>
2021-05-29 19:59:33 +0900

    add

--- Baekjoon/Baekjoon/10816.cpp
+++ Baekjoon/Baekjoon/10816.cpp
@@ -7,6 +7,9 @@ int n, m, temp;
 vector<int> vec;
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> temp;
---
+++ Baekjoon/Baekjoon/1654.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+long long maximum = 0, l, r, ans = 0;
+vector<int> vec;
+
+int main() {
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++) {
+		long long temp;
+		cin >> temp;
+		vec.push_back(temp);
+		maximum = max(maximum, temp);
+	}
+	l = 1, r = maximum;
+
+	// 이분탐색
+	while (r >= l) {
+		long long len = (r + l) / 2;
+		int num = 0;
+		for (int i = 0; i < k; i++)
+			num += vec[i] / len;
+		if (num < n) // 개수가 목표치보다 작을 경우 자르는 길이를 줄여서 개수를 늘림
+			r = len - 1;
+		else { // 개수가 목표치 이상일 경우 자르는 길이를 늘려서 시도
+			l = len + 1;
+			ans = max(ans, len);
+		}
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
